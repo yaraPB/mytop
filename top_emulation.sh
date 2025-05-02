f03d22a279dbbcf70129f292a68550ea4b3ef479
yaraPB <yarakouttane234@gmail.com>
2025-05-02 22:45:07 +0100

    added CPU usage time

--- top_emulation.sh
+++ top_emulation.sh
@@ -3,10 +3,10 @@
 while true; do
     clear
 
-    echo "PID     USER       %CPU    %MEM    COMMAND"
+    echo "PID USER     PRI %CPU %MEM COMMAND   TIME+"
     echo "----------------------------------------------"
 
-    ps -eo pid,user,pcpu,pmem,comm --sort=-pcpu | head -n 11
+    ps -eo pid,user,pri,pcpu,pmem,comm,time --sort=-pcpu | head -n 11
 
     sleep 1
 done
